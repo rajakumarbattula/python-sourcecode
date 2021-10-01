124bb3c242608c04ff6471e8808c00f434384e06
rajakumarbattula <76727150+rajakumarbattula@users.noreply.github.com>
2021-10-01 12:10:21 +0530

    Updated fibo.py
    logic changes to get first ten series of fibonacci numbers

--- fibo.py
+++ fibo.py
@@ -1,6 +1,16 @@
-a,b=0,1
-while b <= 10:
-   print('fiboancci numbers of <10 are : ', b)
-   c=a+b
-   a=b
-   b=c
+a, b = 0, 1
+count=0
+n=int(input(' enter any any number : '))
+if n <= 0:
+   print('number is invalid')
+elif n == 1:
+   print('fibonacci numbers are : ',n,':')
+   print(a)
+else:
+   print('fibanacci numbers are : ')
+   while count <= 10:
+       print('fiboancci numbers are : ', a)
+       c=a+b
+       a=b
+       b=c
+       count += 1
